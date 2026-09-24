c9e690c966b16a5f3a83a6511921947a4456d999
krupa <krupajethwa123@gmail.com>
2026-09-25 00:27:08 +0530

    code updated for app.py

---
+++ app.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from agent.agentic_workflow import GraphBuilder
+from utils.save_to_document import save_document
+from starlette.responses import JSONResponse
+import os
+import datetime
+from dotenv import load_dotenv
+from pydantic import BaseModel
+load_dotenv()
+
+app = FastAPI()
+
+
+class QueryRequest(BaseModel):
+    "query":str
+
+@app.post("/query"):
+async def query_travel_agent(query:QueryRequest):
+    try:
+        print(query)
+        graph = GraphBuilder(model_provider="groq")
+        react_app = graph()
+
+        png_graph = react_app.get_graph().draw_mermaid_png()
+        with open("my_graph.png", "wb") as f:
+            f.write(png_graph)
+
+        print(f"Graph saved as 'my_graph.png' in {os.getcwd()}")
+
+        messages = {"messages": [query.question]}
+        output = react_app.invoke(messages)
+
+
+        # If result is dict with messages:
+        if isinstance(output, dict) and "messages" in output:
+            final_output = output["messages"][-1].content  # Last AI response
+        else:
+            final_output = str(output)
+        
+        return {"answer": final_output}
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
+
